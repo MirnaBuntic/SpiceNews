1a6db2a49a56df887d7cb55fed1bf3a38e1cf683
MirnaBuntic <mirnacb@hiof.no>
2025-09-18 22:55:44 +0200

    satt upp hemsidan

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Layout from './components/Layout';
+import './styles/global.css';
+
+
+export default function App() {
+
+  return (
+    <Layout></Layout>
+  )
+}
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import chili1 from '../img/chili1.png';
+
+export default function Header() {
+
+    return (
+        <header>
+            <div className='logo'>
+                <img src={chili1} alt='En chilli'></img>
+                <p>SpiceNews</p>
+            </div>
+        </header>
+    )
+}
