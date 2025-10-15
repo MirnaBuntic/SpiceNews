a465504dc6da3d9f8fc0ba0d80ce615c93dbe14f
MirnaBuntic <mirnacb@hiof.no>
2025-10-15 17:11:56 +0200

    .

--- src/components/Articles/Trond.jsx
+++ src/components/Articles/Trond.jsx
@@ -3,6 +3,8 @@ import ImgScrollSwap from "../Effects/ImgScrollSwap"
 import DollarBill from "../../img/Trond/DollarBill_Trond.jpg"
 import Busk1 from "../../img/Trond/Busk1.png"
 import Busk2 from "../../img/Trond/Busk2.png"
+import Lommelykt1 from "../../img/Trond/Lommelykt1.png"
+import Lommelykt2 from "../../img/Trond/Lommelykt2.png"
 import "../../styles/_trond.scss"
 import "../../styles/_imgscrollswap.scss"
 
@@ -14,12 +16,17 @@ export default function Trond() {
             <ImgScrollSwap
                 image1={Busk1}
                 image2={Busk2}
+                overlay1={Lommelykt1}
+                overlay2={Lommelykt2}
                 alt1="Busk nr 1"
                 alt2="Busk nr 2"
+                altOverlay1="Lommelykt av"
+                altOverlay2="Lommelykt på"
                 flipCount={8}
                 flipSpeed={0.1}
-                delayScroll={0.3}
-                className="my-custom-class"
+                delayScroll={0.1}
+                overlayScrollOffset={0.2}
+                className="my-flipbook"
             />
 
             
--- src/components/Effects/ImgScrollSwap.jsx
+++ src/components/Effects/ImgScrollSwap.jsx
@@ -8,22 +8,32 @@ gsap.registerPlugin(ScrollTrigger);
 export default function ImgScrollSwap({
   image1,
   image2,
-  alt1 = "Bild 1",
-  alt2 = "Bild 2",
+  overlay1,
+  overlay2,
+  alt1,
+  alt2,
+  altOverlay1,
+  altOverlay2,
   flipCount = 10,
   flipSpeed = 0.1,
   delayScroll = 0.2,
+  overlayScrollOffset = 0.2,
   className = "",
 }) {
   const containerRef = useRef(null);
   const img1Ref = useRef(null);
   const img2Ref = useRef(null);
+  const overlay1Ref = useRef(null);
+  const overlay2Ref = useRef(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
         
         gsap.set(img1Ref.current, { zIndex: 1 });
         gsap.set(img2Ref.current, { zIndex: 0 });
+        gsap.set(overlay1Ref.current, { autoAlpha: 0 });
+        gsap.set(overlay2Ref.current, { autoAlpha: 0 });
+
 
         const tl = gsap.timeline({
         scrollTrigger: {
@@ -32,24 +42,29 @@ export default function ImgScrollSwap({
             end: "bottom top",
             scrub: true,
             pin: true,
-        },
+           },
         });
 
         tl.to({}, { duration: delayScroll });
 
         for (let i = 0; i < flipCount; i++) {
-        tl.set(img1Ref.current, { zIndex: 1 });
-        tl.set(img2Ref.current, { zIndex: 0 });
-        tl.to({}, { duration: flipSpeed });
+            tl.set(img1Ref.current, { zIndex: 1 });
+            tl.set(img2Ref.current, { zIndex: 0 });
+            tl.to({}, { duration: flipSpeed });
 
-        tl.set(img1Ref.current, { zIndex: 0 });
-        tl.set(img2Ref.current, { zIndex: 1 });
-        tl.to({}, { duration: flipSpeed });
+            tl.set(img1Ref.current, { zIndex: 0 });
+            tl.set(img2Ref.current, { zIndex: 1 });
+            tl.to({}, { duration: flipSpeed });
         }
+
+        tl.to(overlay1Ref.current, { autoAlpha: 1, duration: 0.5 });
+        tl.to(overlay2Ref.current, { autoAlpha: 1, duration: 0.5 }, `+=${overlayScrollOffset}`);
+        tl.to(overlay1Ref.current, { autoAlpha: 0, duration: 0.5 }, `<`);
+
     }, containerRef);
 
     return () => ctx.revert();
-    }, [flipCount, flipSpeed, delayScroll]);
+    }, [flipCount, flipSpeed, delayScroll, overlayScrollOffset]);
 
 
   return (
@@ -58,13 +73,26 @@ export default function ImgScrollSwap({
         ref={img1Ref}
         src={image1}
         alt={alt1}
-        style={{ backfaceVisibility: "hidden", willChange: "opacity" }}
-      />
+        />
+
       <img
         ref={img2Ref}
         src={image2}
         alt={alt2}
-        style={{ backfaceVisibility: "hidden", willChange: "opacity" }}
+      />
+
+      <img
+        ref={overlay1Ref}
+        src={overlay1}
+        alt={altOverlay1}
+        className="overlay-image"
+      />
+
+      <img
+        ref={overlay2Ref}
+        src={overlay2}
+        alt={altOverlay2}
+        className="overlay-image"
       />
     </div>
   );
