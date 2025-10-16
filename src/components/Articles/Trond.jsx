a6c562148966e96f9162d6033d3bddc3d153e798
MirnaBuntic <mirnacb@hiof.no>
2025-10-16 15:47:46 +0200

    .

--- src/components/Articles/Trond.jsx
+++ src/components/Articles/Trond.jsx
@@ -1,12 +1,14 @@
 import HeroScroll from "../Effects/HeroScroll"
 import ImgScrollSwap from "../Effects/ImgScrollSwap"
+import ScrollVideo from "../Effects/ScrollVideo"
 import DollarBill from "../../img/Trond/DollarBill_Trond.jpg"
 import Busk1 from "../../img/Trond/Busk1.png"
 import Busk2 from "../../img/Trond/Busk2.png"
 import Lommelykt1 from "../../img/Trond/Lommelykt1.png"
 import Lommelykt2 from "../../img/Trond/Lommelykt2.png"
+import Avis_animasjon from "../../img/Trond/Avis_animasjon_final.mp4"
 import "../../styles/_trond.scss"
-import "../../styles/_imgscrollswap.scss"
+
 
 
 export default function Trond() {
@@ -18,20 +20,41 @@ export default function Trond() {
                 image2={Busk2}
                 overlay1={Lommelykt1}
                 overlay2={Lommelykt2}
-                text="STOP, POLITI!"
+                text="STOPP, POLITI!"
                 alt1="Busk nr 1"
                 alt2="Busk nr 2"
                 altOverlay1="Lommelykt av"
                 altOverlay2="Lommelykt på"
                 flipCount={2}
-                flipSpeed={0.1}
+                flipSpeed={0.5}
                 delayScroll={0.1}
-                overlayScrollOffset={0.2}
+                overlayScrollOffset={0.5}
                 className="my-flipbook"
             />
 
+            <article className="story-intro">
+                <p>Lommelykten lyser i ansiktet på de to fengselsrømmlingene.</p>
+
+                <p>Med bagger fulle av smuglet narkotika befinner seg Trond og Morten nå i et dilemma.</p>
+
+                <p>Overgi seg eller fortsette på rømmen med 2 politibetjente som gisler?</p>
+            </article>
+
+            <ScrollVideo 
+                src={Avis_animasjon}
+                className="my-scroll-video"
+                playbackRate={0.8}
+            />
+
+            <article className="question">
+                <h2>Men hvordan havnet de her?</h2>
+            </article>
+
+            <article className="interview">
+
+            </article>
             
-             <article className="dollarBill-section">
+            <article className="dollarBill-section">
                <h2>"100kr"</h2>
                <img src={DollarBill} alt="Trond på en hundralapp" />
             </article>
---
+++ src/components/Effects/ScrollVideo.jsx
@@ -0,0 +1,49 @@
+import { useRef, useEffect } from "react"
+import gsap from "gsap"
+import  { ScrollTrigger } from "gsap/ScrollTrigger"
+import "../../styles/_scrollvideo.scss"
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function ScrollVideo({ src, className = "", playbackRate = 1 }) {
+    const videoRef = useRef(null)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    const video = videoRef.current
+    const container = containerRef.current
+    if (!video || !container) return
+
+    video.playbackRate = playbackRate
+
+    const trigger = ScrollTrigger.create({
+      trigger: container,
+      start: "top+=10 center",   
+      end: "bottom center",  
+      onEnter: () => video.play(),
+      onEnterBack: () => video.play(),
+      onLeave: () => video.pause(),
+      onLeaveBack: () => video.pause(),
+    })
+
+    return () => {
+      trigger.kill()
+    }
+  }, [])
+
+  return (
+    <article
+      ref={containerRef}
+      className={`scroll-video-container ${className}`}
+    >
+      <video
+        ref={videoRef}
+        src={src}
+        muted
+        playsInline
+        preload="auto"
+        className="scroll-video"
+      />
+    </article>
+  )
+}
