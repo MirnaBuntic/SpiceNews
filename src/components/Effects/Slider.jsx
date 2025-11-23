6f0c11d43fb859bc0fdaa48e4cfe8fb8e61aef4d
MirnaBuntic <mirnacb@hiof.no>
2025-11-24 00:04:24 +0100

    ny slider

--- src/components/Effects/Slider.jsx
+++ src/components/Effects/Slider.jsx
@@ -1,72 +1,34 @@
-import { useState, useLayoutEffect, useRef } from "react"
-import { gsap } from "gsap";
+import { useState} from "react"
 
 export default function Slider ({ images }) {
     const [current, setCurrent] = useState(0);
-    const [direction, setDirection] = useState(1);
-    const imgRef = useRef(null);
-    const touchStart = useRef(null);
-    const touchEnd = useRef(null);
+
+    if (!images.length) return null
 
     const nextSlide = () => {
-        setDirection(1);
         setCurrent((prev) => (prev + 1) % images.length);
     };
 
     const prevSlide = () => {
-        setDirection(-1);
         setCurrent((prev) => (prev - 1 + images.length) % images.length);
     };
 
-    const onTouchStart = (e) => (touchStart.current = e.touches[0].clientX);
-    const onTouchEnd = (e) => {
-        touchEnd.current = e.changedTouches[0].clientX;
-        handleSwipe();
-    };
-
-    const handleSwipe = () => {
-        const distance = touchStart.current - touchEnd.current;
-        const minSwipeDistance = 50;
-        if (distance > minSwipeDistance) nextSlide();
-        else if (distance < -minSwipeDistance) prevSlide();
-    };
-
-    useLayoutEffect(() => {
-        const img = imgRef.current;
-
-        gsap.fromTo(
-            img,
-            { x: direction * 100, autoAlpha: 0 },
-            { x: 0, autoAlpha: 1, duration: 0.6, ease: "power2.out" }
-        );
-    }, [current]);
-
+    
     return (
-        <article
-            className="slider"
-            onTouchStart={onTouchStart}
-            onTouchEnd={onTouchEnd}
-            >
-            <button
-                onClick={prevSlide}
-                className="slider-button prev"
-            >
-                &#10094;
+        <article className="slider">
+            <button onClick={prevSlide} className="slider-button prev">
+                {"<"}
             </button>
 
             <img
-                ref={imgRef}
                 src={images[current].src}
                 alt={images[current].alt}
                 className="slider-image"
                 draggable={false}
             />
 
-            <button
-                onClick={nextSlide}
-                className="slider-button next"
-            >
-                &#10095;
+            <button onClick={nextSlide} className="slider-button next">
+                {">"}
             </button>
 
             {images[current].caption && (
